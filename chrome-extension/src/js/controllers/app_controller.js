7445a70f90240344ccf9f7a8758c696959008c8a
smhabibjr <smhabib.abir2030@gmail.com>
2026-04-30 21:07:04 +0600

    add signin and entries frames

---
+++ chrome-extension/src/js/controllers/app_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+class AppController extends Controller {
+  connect() {
+    console.log("app controller connected")
+    const token = false;
+
+    if (token) {
+        Turbo.visit("/frames/entries.html", { frame: "app" })
+    }
+  }
+}
+
+export default AppController;
--- chrome-extension/src/js/controllers/index.js
+++ chrome-extension/src/js/controllers/index.js
@@ -1,1 +1,4 @@
 import { application } from '../application'
+import AppController from './app_controller'
+
+application.register("app", AppController)
